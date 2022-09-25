cfb52a4c37cfc62adca2f2f3633bf4f1ea58bc99
BesantJ2ee <simonjustin321@gmail.com>
2022-09-25 09:59:51 +0530

    yesterdays update

--- src/java_Sep_18/StringDiscussion.java
+++ src/java_Sep_18/StringDiscussion.java
@@ -1,12 +1,57 @@
 package java_Sep_18;
 
 public class StringDiscussion {
+	
+	Integer abc()
+	{
+		
+		
+	return 7;	
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		String a = "Kolkata";
-		System.out.println(a.length());		
+		String output = "Kolata";
+		System.out.println(a.length());
+		
+		
+		String[] vb = a.split("");
+		
+		//System.out.println(vb.length);
+		
+		for(int i=0;i<vb.length;i++)
+		{
+			if(i==4 && i==3)
+			{
+				
+				System.out.print(vb[i]);	
+			}
+			
+		}
+		
+		//System.out.println(vb);
+		
+		String sdf="7";
+		//System.out.println(sdf);
+		
+		
+		String acv=null;
+		//System.out.println(acv.length());
+		
+		//	Reserved keyword cannot use for your own purpose
+		//String void = "cvb";
+		//String static ="sgsfs;"
+	    //String public ="ffsgsfs";
+		
+		
+		
+		
+		
+		
 
 	}
 
